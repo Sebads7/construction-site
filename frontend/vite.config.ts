098e71a38237e151cfe8f4e1d3027c759f34f87b
Sebas <disalvatore.sebastian@gmail.com>
2024-08-03 19:50:03 -0300

    working on deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,14 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { SourceMap } from "module";
 
 export default defineConfig({
   plugins: [react()],
 
+  build: {
+    outDir: "dist",
+  },
   server: {
     host: "192.168.68.50",
     port: 5173,
