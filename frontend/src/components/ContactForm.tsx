1e35fb8e04595a065b041ccb2486c29086363fc5
Sebas <disalvatore.sebastian@gmail.com>
2024-08-03 19:09:15 -0300

    running build command to deploy

--- frontend/src/components/ContactForm.tsx
+++ frontend/src/components/ContactForm.tsx
@@ -2,7 +2,7 @@
 
 // IMPORTS FROM ZOD AND REACT-HOOK-FORM
 import { zodResolver } from "@hookform/resolvers/zod";
-import { set, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { z } from "zod";
 
 // IMPORTS FROM SHADCN COMPONENTS
